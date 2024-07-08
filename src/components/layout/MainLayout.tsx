45d8190a27844b4d4122af0006c621f226f4cc85
Your Name <arfathossen541@gmail.com>
2024-07-08 13:33:58 +0600

    student admin faculty routes and sidebaritems basic seti

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,53 +1,12 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
-import { facultySidebarItems } from "../../routes/faculty.routes";
-import { studentSidebarItems } from "../../routes/student.routes";
-const { Header, Content, Footer, Sider } = Layout;
+import Sidebar from "./Sidebar";
+const { Header, Content } = Layout;
 
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>PH University</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
-        />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={facultySidebarItems}
-        />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={studentSidebarItems}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
@@ -60,9 +19,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
--- src/routes/student.routes.tsx
+++ src/routes/student.routes.tsx
@@ -1,25 +1,15 @@
 import { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
 import MySchedule from "../pages/student/MySchedule";
 import OfferedCourse from "../pages/student/OfferedCourse";
 import StudentDashboard from "../pages/student/StudentDashboard";
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
-type TSidebarItem = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItem[];
-};
 
 type TStudentPath = {
   name: string;
-  path: string;
-  element: ReactNode;
+  path?: string;
+  element?: ReactNode;
   children?: TStudentPath[];
 };
+
 export const studentPaths: TStudentPath[] = [
   {
     name: "Dashboard",
@@ -27,58 +17,18 @@ export const studentPaths: TStudentPath[] = [
     element: <StudentDashboard />,
   },
   {
-    name: "Offered Course",
-    path: "offered-course",
-    element: <OfferedCourse />,
-  },
-  {
-    name: "My Schedule",
-    path: "schedule",
-    element: <MySchedule />,
+    name: "Student Management",
+    children: [
+      {
+        name: "Offered Course",
+        path: "offered-course",
+        element: <OfferedCourse />,
+      },
+      {
+        name: "My Schedule",
+        path: "my-schedule",
+        element: <MySchedule />,
+      },
+    ],
   },
 ];
-
-export const studentSidebarItems = studentPaths.reduce(
-  (acc: TSidebarItem[], item) => {
-    if (item.path && item.name) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/student/${item.path}`}>{item.name}</NavLink>,
-      });
-    }
-
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/student/${child.path}`}>{child.name}</NavLink>,
-        })),
-      });
-    }
-
-    return acc;
-  },
-  []
-);
-
-export const studentRoutes = studentPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
-
-  return acc;
-}, []);
