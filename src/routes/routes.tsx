07d7749d8c04e264e979284102e5e3728a244d82
Your Name <arfathossen541@gmail.com>
2024-07-08 12:18:30 +0600

    create a reuseable routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import { adminRoutes } from "./admin.routes";
+import { routeGenerator } from "../utils/routesGenerator";
+import { adminPaths } from "./admin.routes";
 import { facultyPaths } from "./faculty.routes";
 import { studentPaths } from "./student.routes";
 
@@ -14,17 +15,17 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: adminRoutes,
+    children: routeGenerator(adminPaths),
   },
   {
     path: "/faculty",
     element: <App />,
-    children: facultyPaths,
+    children: routeGenerator(facultyPaths),
   },
   {
     path: "/student",
     element: <App />,
-    children: studentPaths,
+    children: routeGenerator(studentPaths),
   },
   {
     path: "/login",
